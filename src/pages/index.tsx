2f1db89480b5383d47710519c52395cca21924be
C Moore <sillypugtastic119@gmail.com>
2024-11-01 13:41:08 -0700

    Button to where I wanted it to be.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,8 +24,8 @@ export default function Home() {
     <div
       className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
-      <div className="flex items-center justify-center h-auto w-auto">
-        <a className="hover:bg-slate-900 ease-in-out flex justify-center items-center shadow-md rounded-xl w-40 h-36 bg-black transform rotate-12" href="">
+      <div className="h-auto w-auto">
+        <a className="hover:bg-slate-900 ease-in-out flex justify-center items-center shadow-md rounded-xl w-40 h-36 bg-black transform rotate-12 translate-y-72" href="">
           <dd className="font-medium font-mono text-white">
             Projects
           </dd>
@@ -39,4 +39,3 @@ export default function Home() {
     </div>
   );
 }
-
