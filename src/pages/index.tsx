37fc9dee3a6efcac03f73f7f70f39641f3f79e76
C Moore <sillypugtastic119@gmail.com>
2024-11-02 09:53:05 -0700

    Projects show, + animations

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import localFont from "next/font/local";
 import React, { Component } from 'react';
+import { useState } from 'react';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,19 +25,29 @@ console.log("%c" + "Your looking in the wrong place you won't get far here!", "c
 console.log("%c" + "Go to my GitHub REPO: https://github.com/thepugmaker/website-redesign", "color: #0077ff; font-size: 24px; font-weight: bold;");
 
 export default function Home() {
+  const [divVisible, setDivVisible] = useState(false);
+
+  const handleButtonClick = () => {
+    setDivVisible(true);
+  };
+
   return (
     <div
       className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
       <div className="mt-[46rem]">
-        <a className="hover:bg-slate-900 flex justify-center items-center shadow-md rounded-xl w-40 h-36 bg-black transition duration-500 animate-wiggle" href="" id="openprojectsbutton">
+        <button className="hover:bg-slate-900 flex justify-center items-center shadow-md rounded-xl w-40 h-36 bg-black transition duration-500 animate-wiggle" id="openprojectsbutton" onClick={handleButtonClick}>
           <dd className="font-medium font-mono text-white">
             Projects
           </dd>
-        </a>
+        </button>
       </div>
-      <div className="" id="projectsdiv">
-
+      <div className={`transition-opacity duration-500 ${divVisible ? 'opacity-100' : 'opacity-0'} fixed top-[10rem] left-[68rem] p-4`} id="projectsdiv">
+        <button className="hover:bg-slate-900 flex justify-center items-center shadow-md rounded-xl w-40 h-36 bg-black transition duration-500 animate-wiggle">
+          <dd className="font-medium font-mono text-white">
+            Astro cat
+          </dd>
+        </button>
       </div>
       <footer>
       </footer>
