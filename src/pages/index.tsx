1d9a9dedc932becb5dbc471a4f56d1e615941ae7
MrPugPug <sillypugtastic119@gmail.com>
2024-11-20 21:46:31 -0800

    New updates to how the project is shown.
    This commit is about the project box to get the name and click a button.
    The animation is broken for the projects button at this time.
    Tried to add title to web page but it didn't work.
    Changed a bit of the design for the main divs.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Head from "next/head";
 import localFont from "next/font/local";
 import React, { Component } from 'react';
 import { useState } from 'react';
@@ -14,7 +15,9 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
-<title>MrPugPug</title>
+<head>
+  <title>MrPugPug</title>
+</head>
 
 console.log("%c" + "Hey you looking through my code???", "color: #0077ff; font-size: 24px; font-weight: bold;");
 console.log("%c" + "Your looking in the wrong place you won't get far here!", "color: #0077ff; font-size: 24px; font-weight: bold;");
@@ -43,17 +46,18 @@ export default function Home() {
           </dd>
         </button>
       </div>
-      <div className={`transition-opacity duration-500 flex items-center ${projectsDiv ? 'opacity-100' : 'opacity-0'} fixed top-[10rem] left-[68rem] `} id="projectsdiv">
-        <div className="rounded-xl flex items-center justify-center dark:bg-slate-600 shadow-md w-96 h-48">
-          <dd className="font-medium font-mono text-white mt-2">
-            Astro cat
-          </dd>
-          <button className="rounded-lg flex justify-center items-center shadow-sm bg-slate-800 hover:bg-black transition duration-500 mt-10" id="astrocatprojectinfo" onClick={handleAstroCatProjectClick}>
-            <dd className="font-small font-mono text-white">
-              More info
-            </dd> 
-          </button>
-        </div>
+      <div className={ `transition-opacity duration-500 ${projectsDiv ? 'opacity-100' : 'opacity-0'} `} id="projectsdiv"></div>
+        <div className="fixed top-[10rem] left-[68rem]" id="astrocatprojectmain">
+          <div className="rounded-xl flex shadow-md w-96 h-48 bg-gradient-to-r from-slate-600 to-slate-800">
+            <dd className="font-medium font-mono justify-self-start text-white mt-20 pl-8">
+              Astro cat
+            </dd>
+            <button className="rounded-lg flex justify-center items-center justify-self-end shadow-xl bg-slate-500 hover:bg-slate-600 transition duration-500 mt-14 ml-24 h-20 w-32" id="astrocatprojectinfo" onClick={handleAstroCatProjectClick}>
+              <dd className="font-small font-mono text-white">
+                Tell me about it!
+              </dd> 
+            </button>
+          </div>
       </div>
     </div>
   );
