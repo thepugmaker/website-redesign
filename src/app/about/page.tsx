d772fc4c3c8f206d5b2af7050a2349a01c4d7ebe
thepugmaker <contact@mrpugpug.com>
2025-07-29 00:17:59 -0700

    Tailwind upgrade
    It told me to verify and commit the changes to my repo

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
         <h1 className="text-black dark:text-white font-medium font-mono rounded-xl">
             Here is some stuff about me!
         </h1>
-        <div className="h-auto w-auto bg-gradient-to-r from-red-500 to-blue-600 justify-items-center rounded-xl m-12 p-6">
+        <div className="h-auto w-auto bg-linear-to-r from-red-500 to-blue-600 justify-items-center rounded-xl m-12 p-6">
             <dd className="text-white font-medium font-mono mt-4">
                 I develop websites, software, and games. My game downloads are at https://puggamedev.itch.io/
             </dd>
--- src/app/projects/colonysim/page.tsx
+++ src/app/projects/colonysim/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
       <h1 className="mt-4 text-black dark:text-white font-medium font-mono">
         Work in progress conlony sim game page
       </h1>
-      <div className="h-auto w-auto bg-gradient-to-r from-red-500 to-blue-600 justify-items-center rounded-xl p-8 m-14">
+      <div className="h-auto w-auto bg-linear-to-r from-red-500 to-blue-600 justify-items-center rounded-xl p-8 m-14">
         <dd className="text-white font-medium font-mono mt-4 ">
             Work in progress, developed with Unity 6. 2D Side view. Smooth, Not a grid.
         </dd>
